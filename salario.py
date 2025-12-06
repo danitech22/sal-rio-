82bc78e161d1adba3e899e96f988a28740136ed8
Daniel Paulino <dleopoldino852@gmail.com>
2025-12-06 12:08:58 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,12 @@
+nome = input("digite o nome")
+salario = float(input("digite o salario"))
+
+
+if salario <=2_000:
+    bonus = salario * 0.10
+else :
+    bonus = salario * 0.5
+
+print("nome",nome)
+print("salario",salario)
+
